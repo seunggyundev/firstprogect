f0738245e64c27da0c97e6b9725ee8f11d00a688
seunggyun_jang <seunggyun2743@gmail.com>
2022-01-10 22:29:33 +0900

    Initial commit

---
+++ src/main/java/com/example/firstprogect/FirstprogectApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstprogect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstprogectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstprogectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/firstprogect/FirstprogectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstprogect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstprogectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
